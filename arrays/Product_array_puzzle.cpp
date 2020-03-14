76c3be8cc1b24973d2f644de528dff2e8cd2f424
Anu Bansal <anubansal9999@gmail.com>
2020-03-14 19:27:06 +0530

    Update Product_array_puzzle.cpp

--- arrays/Product_array_puzzle.cpp
+++ arrays/Product_array_puzzle.cpp
@@ -1,4 +1,4 @@
-//https://www.geeksforgeeks.org/a-product-array-puzzle/
+// Problem: https://www.geeksforgeeks.org/a-product-array-puzzle/
 #include<iostream>
 using namespace std;
 int main()
@@ -31,6 +31,45 @@ int T, n;
     }
     for(int i=0; i<n; i++) {
         prod[i] = left[i]*right[i];
+        cout<<prod[i]<<" ";
+    }
+    cout<<endl;
+	}
+	return 0;
+}
+
+//Method 2: Space Complexity-O(1)
+#include<iostream>
+using namespace std;
+int main()
+ {
+int T, n;
+	cin>>T;
+	int i, j; 
+	for (int k=0; k<T; k++)
+	{
+	    cin>>n;
+	    int arr[n];
+	    for(int j=0; j<n; j++)
+	    {
+	        cin>>arr[j]; 
+	    }
+	    if (n == 1) { 
+        cout << 0; 
+        return 0;
+    }
+    int* prod = new int[sizeof(int) * n]; 
+    int temp = 1; 
+    for(int i=0; i<n; i++) {
+        prod[i] = temp;
+        temp = temp*arr[i];
+    }
+    temp = 1;
+    for(int j=n-1; j>=0; j--) {
+        prod[j] = prod[j]*temp;
+        temp = temp*arr[j];
+    }
+    for(int i=0; i<n; i++) {
         cout<<prod[i]<<" ";
     }
     cout<<endl;
