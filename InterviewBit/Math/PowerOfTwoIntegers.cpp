c90c5db1741b0359863ab8e3ac45f40e8032b6de
Anu Bansal <anubansal9999@gmail.com>
2020-04-19 14:51:52 +0530

    Update PowerOfTwoIntegers.cpp

--- InterviewBit/Math/PowerOfTwoIntegers.cpp
+++ InterviewBit/Math/PowerOfTwoIntegers.cpp
@@ -1,4 +1,4 @@
-/**
+/** Problem Statement:
 *
 * Given a positive integer which fits in a 32 bit signed integer, find if it can be expressed as A^P where P > 1 and A > 0.
 * A and P both should be integers.
