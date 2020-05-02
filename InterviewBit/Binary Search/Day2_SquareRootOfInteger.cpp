c63c626bb6e3e290f9eac030b5ecde22cec2e143
Anu Bansal <anubansal9999@gmail.com>
2020-05-02 20:50:17 +0530

    Create Day2_SquareRootOfInteger.cpp

---
+++ InterviewBit/Binary Search/Day2_SquareRootOfInteger.cpp
@@ -0,0 +1,22 @@
+// Problem: https://www.interviewbit.com/problems/square-root-of-integer/
+// Tip: mid*mid gives overflow error, so to avoid that I declared mid as long
+int Solution::sqrt(int A) {
+    int l = 1;
+    int r = A;
+    int ans = 0;
+    while(l<=r){
+        long int mid = l + (r-l)/2;
+        if(2l * mid*mid == A){
+            ans = mid;
+           
+            return ans;
+        } else if(mid*mid > A){
+            
+            r = mid - 1;
+        } else{
+            ans = mid;
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
