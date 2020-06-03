7a7184b5fa10b5e3012f61a54abce6e49222f562
Anu Bansal <anubansal9999@gmail.com>
2020-06-03 23:50:02 +0530

    Update Day33_TwoCityScheduling.cpp

--- leetcode/June Challenge/Day33_TwoCityScheduling.cpp
+++ leetcode/June Challenge/Day33_TwoCityScheduling.cpp
@@ -1,4 +1,5 @@
 // https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3349/
+// M1 - O(nlogn) time complexity and O(n) space Complexity
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
@@ -20,4 +21,27 @@ public:
         }
         return ans;
     }
+};
+
+//M2 - O(nlogn) Time Complexity and O(1) Space Complexity
+bool comp(vector<int> x,vector <int> y) {
+        return (x[0]-x[1]) < (y[0] - y[1]);
+    }
+class Solution {
+    
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        int ans=0;
+        sort(costs.begin(), costs.end(),comp);
+        for (int i=0; i<n; i++) {
+            if(i<n/2) {
+                ans += costs[i][0];
+            } else{
+                ans += costs[i][1];
+            }
+        }
+        return ans;
+    }
+   
 };
