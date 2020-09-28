dbf98d10ccc2de6bb609ec1c55eeb072f0635d46
Anu Bansal <anubansal9999@gmail.com>
2020-09-28 23:03:23 +0530

    Create 12_RotateMatrix.cpp

---
+++ RoadMap/12_RotateMatrix.cpp
@@ -0,0 +1,13 @@
+/*
+Approach 1: Brute Force: T.C- O(n^2), S.C - O(n^2)
+- Make a new matrix and keep filling its columns from last with rows of first matrix from begining
+Approach 2: T.C- O(n^2), S.C- O(1)
+- First do transpose of the given matrix
+- Then reverse the rows and boom, matrix would be inverted
+*/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+    }
+};
