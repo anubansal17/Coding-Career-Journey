cceb4e160d797ad11b163088d482c943b5aa80f9
Anu Bansal <anubansal9999@gmail.com>
2020-10-03 19:47:08 +0530

    Update 40_4Sum.cpp

--- RoadMap/40_4Sum.cpp
+++ RoadMap/40_4Sum.cpp
@@ -19,7 +19,44 @@ Approach 2: Two loops + two pointers, T.C - O(n^3), S.C- O(1)
 */
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
+    vector<vector<int> > Solution::fourSum(vector<int> &A, int B) {
+    int n = A.size();
+    vector<vector<int>> ans;
+    sort(A.begin(), A.end());
+    for (int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            int target = B - A[i] - A[j];
+            int left = j+1;
+            int right = n-1;
+            while(left<right) {
+                if(A[left]+A[right] < target) {
+                    left++;
+                }
+                else if(A[left]+A[right] > target) {
+                    right --;
+                }
+                else {
+                   ans.push_back({A[i], A[j], A[left], A[right]});
+                   int leftEle = A[left];
+                   int rightEle = A[right];
+                    while(left<right && A[left] == leftEle) {
+                        left++;
+                    }
+                    while(left<right && A[right] == rightEle) {
+                        right--;
+                    }
+                }
+            }
+            // Jumping thru the duplicates
+            while(j+1<n && A[j] == A[j+1]) {
+                j++;
+            }
+        }
+        // Jumping thru the duplicates
+        while(i+1<n && A[i] == A[i+1]) {
+                i++;
+            }
     }
+    return ans;
+}
 };
