4dffc9eb6d6d87130c521483ef59bdad8423919e
Anu Bansal <anubansal9999@gmail.com>
2020-10-03 14:44:42 +0530

    Create 40_4Sum.cpp

---
+++ RoadMap/40_4Sum.cpp
@@ -0,0 +1,24 @@
+/*
+Approach 1: Three loops + Binary Search T.C - O(n^3Logn(n)), S.C- O(1)
+Approach 2: Two loops + two pointers, T.C - O(n^3), S.C- O(1)
+- for i= 0 to n-1
+-   for j = i+1 to n-1
+-       left = j+1
+-       right = n-1
+-       target2 = target - nums[i] - nums[j]
+-       while(left < right){
+-       if(nums[left]+nums[right] < target2) left++;
+-       else if(nums[left] + nums[right] > target2) right--;
+-       else {
+-       add these elements as quadruplet to answer
+-       increment the left to next distinct element
+-       increment the right to next distinct element
+-   }
+-       }
+*/
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+    }
+};
