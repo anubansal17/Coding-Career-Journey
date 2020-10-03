ef60ade3aaa57c2f33bb7031e8ebd4d2f3535c12
Anu Bansal <anubansal9999@gmail.com>
2020-10-03 19:44:34 +0530

    Update 40_4Sum.cpp

--- RoadMap/40_4Sum.cpp
+++ RoadMap/40_4Sum.cpp
@@ -1,6 +1,7 @@
 /*
 Approach 1: Three loops + Binary Search T.C - O(n^3Logn(n)), S.C- O(1)
 Approach 2: Two loops + two pointers, T.C - O(n^3), S.C- O(1)
+- Sort the vector given
 - for i= 0 to n-1
 -   for j = i+1 to n-1
 -       left = j+1
