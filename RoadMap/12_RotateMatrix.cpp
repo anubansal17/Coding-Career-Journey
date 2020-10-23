71f4f614e05b770b013a6972668f850126aee546
Anu Bansal <anubansal9999@gmail.com>
2020-10-23 23:14:43 +0530

    Update 12_RotateMatrix.cpp

--- RoadMap/12_RotateMatrix.cpp
+++ RoadMap/12_RotateMatrix.cpp
@@ -8,6 +8,16 @@ Approach 2: T.C- O(n^2), S.C- O(1)
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        
+        int n = matrix.size();
+        for (int i=0; i<n; i++) {
+            for (int j=i; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0; i<n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
