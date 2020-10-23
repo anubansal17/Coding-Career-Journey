0ed3b6d19c9943bc701cf10d31e56597863d6192
Anu Bansal <anubansal9999@gmail.com>
2020-10-23 21:56:04 +0530

    Update 9_NextPermutation.cpp

--- RoadMap/9_NextPermutation.cpp
+++ RoadMap/9_NextPermutation.cpp
@@ -2,13 +2,13 @@
 Approach 1: Brute force, T.C - O(n!)
 Approach 2: Optimized one, T.C - O(n)
 - Base condition: Check if it is the highest permutation possible with these numbers
-- In dictionary, the right part is always an increasing sequence, so we just need to find the point where this increasing sequence is ending
-- For this, Traverse from left and check for first a[i] < a[i+1] - index1
+- In dictionary, the right part is always an increasing sequence, so we just need to find the point where this increasing sequence is stopping
+- For this, Traverse from right and check for first a[i] < a[i+1] - index1
 - Now, traverse from left and check the smallest number which is grater than nums[index1] - index2
 - Swap nums[index1] with nums[index2]
 - Reverse this increasing sequence to have the lowest combination of numbers according to dictionary
 */
-void swap(vector<int> &nums, int i, int j){
+void swap(vector <int> &nums, int i, int j) {
     int temp = nums[i];
     nums[i] = nums[j];
     nums[j] = temp;
@@ -16,31 +16,31 @@ void swap(vector<int> &nums, int i, int j){
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int index1 = -1;
         int n = nums.size();
-        int index = 0;
-        // Check if the size of the array is 1
-        if(n == 1) {
-            return;
-        }
-        // check if its the highest permutation possible
-        if(nums.size() > 1 && nums[0]>nums[nums.size()-1]){
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        for (int i=0; i<n-1; i++) {
-            if(nums[i] < nums[i+1]) {
-                index = i;
-            }
-        }
-        for (int i = n-1; i >= 0; i--) {
-            // Finding the smallest element at the right side which is greater than the breaking point and break the loop there
-            if(nums[i] > nums[index]) {
-                swap(nums, i, index);
+        // first element which is lesser than previous element starting from right
+        for (int i = n-2; i>=0; i--) {
+            if(nums[i]<nums[i+1]) {
+                index1 = i;
                 break;
             }
         }
-        if(index != n-1) {
-         reverse(nums.begin()+index+1, nums.end());   
+        if(index1 == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
         }
+        //cout<<index1;
+        int index2 = index1;
+        // first element from right which is greater than element present at index1
+        for (int i=n-1; i>index1; i--) {
+            if(nums[i] > nums[index1]) {
+                index2 = i;
+                break;
+            }
+        }
+        //cout<<index2;
+       // swap both the elements
+        swap(nums, index1, index2);
+        reverse(nums.begin()+index1+1, nums.end());
     }
 };
