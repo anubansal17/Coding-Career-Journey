f34cb330d94dbc194dc1dc98ee16dc9a4241f08f
Anu Bansal <anubansal9999@gmail.com>
2020-10-24 11:17:25 +0530

    Update 14_Pow(x,n).cpp

--- RoadMap/14_Pow(x,n).cpp
+++ RoadMap/14_Pow(x,n).cpp
@@ -1,29 +1,29 @@
-int calcPower(long int x, int n, int d){
-    long long int ans = 1;
-     if(n == 1 ){
-        return x;
-    }
-    if (n == 0){
-        return 0;
-    }
-    if(n%2 == 0){
-       ans =  calcPower((x+x)%d, n/2, d)%d; 
-    }else{
-        n = n-1;
-        ans =  calcPower(x, n/2, d)%d;
-        ans = (ans%d + ans%d + x%d)%d;
-    }
+// edge case can be there when n = INT_MIN
+double pow(double x, int n) {
+        // Base condition
+        if(n == 0) {
+            return 1;
+        }
+        else if(n == 1) {
+            return x;
+        }
+
+        double ans = 1;
+        if(n%2 == 0) {
+            ans = pow(x*x, n/2);
+        } else {
+            ans = pow(x*x, n/2);
+            ans = ans * x;
+        }
     return ans;
 }
-int Solution::pow(int x, int n, int d) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    //x ^ n = (x * x) ^( n/2)
-    long int long ans = calcPower(x, n, d);
-    if(ans<0){
-        return (d+ans)%d;
+class Solution {
+public:
+    double myPow(double x, int n) {
+    double ans = pow(x, n);
+        if(n < 0) {
+            return 1/ans;
+        }
+        return ans;
     }
-    return ans%d;
-}
+};
