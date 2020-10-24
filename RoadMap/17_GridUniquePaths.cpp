2f7205d47a2d84f39b5c31942b34c93ef656c7cd
Anu Bansal <anubansal9999@gmail.com>
2020-10-24 13:50:20 +0530

    Update 17_GridUniquePaths.cpp

--- RoadMap/17_GridUniquePaths.cpp
+++ RoadMap/17_GridUniquePaths.cpp
@@ -7,55 +7,49 @@ Approach 1: Recursive solution T.C- exponential, S,C - exponential(Stack calls)
 - If(i>=m || j>=n) return 0;
 - if( i== m-1 and j = n-1) return 1;
 - return countPaths(i+1,j)+countPaths(i,j+1)
-Approach 2: DP T.C - O(n^2), S.C- O(n^2)
+Approach 2: DP T.C - O(n*m), S.C- O(n*m)
 - Initialise the matrix with -1
 - Store the answer in tp[i][j] for current index
 - Before callling the recursive fxn, check if it is already present or not
 Apptoach 3: Using combinatorics T.C- O(m-1) or O(n-1), S.C - O(1)
-- Total number of directions for moving right words = n-1
+- Total number of directions for moving rightwards = n-1
 - Total number of directions for moving downwards = m-1( means number of rows - 1)
 - Total directions = n-1 + m-1 = m+n-2
 - We just need to choose n-1 unique rightward directions or m-1 unique downward directions
 - Answer would be = m+n-2Cm-1 or m+n-2Cn-1
 */
-// Approach 3 Implementation
-class Solution {
+// Approach 3 Implementation - Most optimized
+/*class Solution {
 public:
     int uniquePaths(int m, int n) {
         int total = m+n-2;
         int r = m-1;
         double ans = 1;
         // calculate totalCr (nCr)
-        for (int i=1; i<=r; i++) {
-            ans *= (double)(total-r+i)/i;
+        for (int i=0; i<r; i++) {
+            ans *= (double)(total-i));
+            ans /= i+1;
         }
         return round(ans);
     }
-};
-// Approach 2: Implementation
-int countPaths(int i, int j, int m, int n, vector<vector<int>> &tp) {
-    // Base condition : If current index exceeds the matrix
-    if(i >= m || j >= n) {
+};*/
+int countPaths(int i, int j, int destA, int destB, vector <vector<int>>&tp) {
+    // Base condition: If i and j exceeds its limits
+    if(i > destA || j > destB){
         return 0;
-    } 
-    // If we reach till end of the matrix, i.e. our destination
-    else if(i == m-1 && j == n-1) {
+    } else if(i == destA and j == destB) {
         return 1;
     }
-    
-    else if(tp[i][j] != -1) {
+    if(tp[i][j] != -1) 
         return tp[i][j];
-    }
-    return tp[i][j] = countPaths(i+1, j, m, n, tp) + countPaths(i, j+1, m, n, tp);
+    return tp[i][j] = countPaths(i+1, j, destA, destB, tp) + countPaths(i, j+1, destA, destB, tp);
     
 }
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> tp(m, vector<int> (n, -1));
-        
-        int ans;
-        ans = countPaths(0, 0, m, n, tp);
+        vector <vector<int>>tp(m, vector<int>(n, -1));
+        int ans = countPaths(0, 0, m-1, n-1, tp);
         return ans;
     }
 };
