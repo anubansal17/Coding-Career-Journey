d0f13ebcf92cb4ca050949d0a89e38e2047030f0
Anu Bansal <anubansal9999@gmail.com>
2020-10-24 11:29:55 +0530

    Update 15_MajorityElement(NdivideBy2).cpp

--- RoadMap/15_MajorityElement(NdivideBy2).cpp
+++ RoadMap/15_MajorityElement(NdivideBy2).cpp
@@ -10,22 +10,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //map <int, int> mp;
         int n = nums.size();
         int count = 1;
-        int curNum = nums[0];
-        for(int i=1; i<n; i++){
-            if(nums[i] == curNum){
-                count ++;
+        int majEle = nums[0];
+        for (int i=1; i<n; i++){
+            if(nums[i] == majEle) {
+                count++;
             } else{
-                if(count == 0){
-                    curNum = nums[i];
-                    count = 1;
-                } else{
-                    count --;
-                }
+                count--;
+            }
+            if(count == 0) {
+                majEle = nums[i];
+                count = 1;
             }
         }
-        return curNum;
+        return majEle;
     }
 };
