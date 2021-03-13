553a16a56dcd19d7233b07b316248f771ee49117
Anu Bansal <anubansal9999@gmail.com>
2021-03-13 17:09:42 +0530

    Update 3_RepeatAndMissingNumberArray.cpp

--- RoadMap/3_RepeatAndMissingNumberArray.cpp
+++ RoadMap/3_RepeatAndMissingNumberArray.cpp
@@ -8,10 +8,15 @@ actualSum
 exSum - missing + dup = actualSum
 miss + dup = actualSum - exSum
 
-Approach 2: expectedSum - ActualSum = missing - dupl
+Approach 2: T.C - O(n) and S.C - O(1)
+expectedSum - ActualSum = missing - dupl
 (1^2+2^2+---+n^2) - Sum(Actual Squares) = missing^2 - dupl^2
 => (expectedSum-ActualSum)(missing + dupl)
+
+Approach 3: Using XOR
 */
+
+// Approach 2 Implementation
 vector<int> Solution::repeatedNumber(const vector<int> &A) {
    int n = A.size();
    int dupl;
@@ -36,3 +41,60 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
    ans.push_back(miss);
    return ans;
 }
+
+// Approach 3 Implementation
+// Using xor
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    // Dupl^missing
+    int n = A.size();
+    int aXor = A[0];
+    for (int i=1; i<n; i++) {
+        aXor ^= A[i];
+    }
+    int eXor = 1;
+    for (int i=2; i<=n; i++) {
+        eXor ^= i;
+    }
+    int pos = -1;
+    // dupl^miss = comb => aXor^eXor
+    int comb = aXor^eXor;
+    // first set bit in comb
+    for(int i=0; i<n; i++){
+        // left shift comb by i
+        if((comb>>i) &1){
+            pos = i;
+            break;
+        }
+    }
+    // bucket of elements which have pos as 1
+    int x = 0;
+    int y = 0;
+    for (int num : A) {
+        if((num>>pos)&1) {
+            x ^= num;
+        }else{
+            y ^= num;
+        }
+    }
+    for(int i=1; i<=n; i++){
+        if((i>>pos)&1){
+            x ^= i;
+        }else {
+            y ^= i;
+        }
+    }
+    vector<int> ans;
+    for(int i=0; i<n; i++) {
+        if(A[i] == x){
+            ans.push_back(x);
+            ans.push_back(y); 
+            break;
+        } else if(A[i] == y) {
+            ans.push_back(y);
+            ans.push_back(x); 
+            break; 
+        }
+    }
+    return ans;
+}
+
