4a17046dfd47646436ee93441b4a4840f67684d2
Anu Bansal <anubansal9999@gmail.com>
2021-05-06 16:19:05 +0530

    Update 13_SearchInA2DMatrix.cpp

--- RoadMap/13_SearchInA2DMatrix.cpp
+++ RoadMap/13_SearchInA2DMatrix.cpp
@@ -36,3 +36,27 @@ public:
         return false;
     }
 };
+
+// 4th Approach Implementation
+class Solution {
+public:
+    bool binarySearch(int start, int end, int target, vector<vector<int>> &matrix, int cols) {
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+            if(matrix[mid/cols][mid%cols] == target)
+                return 1;
+            else if(matrix[mid/cols][mid%cols] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return 0;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = 0;
+        int col = n-1;
+        return binarySearch(0, n*m-1, target, matrix, n);
+    }
+};
