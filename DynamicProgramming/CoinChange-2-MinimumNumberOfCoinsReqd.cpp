aa6e1474cc6f92a3c876a8780bb58ec9ddd08368
Anu Bansal <anubansal9999@gmail.com>
2022-01-22 22:53:29 +0530

    Update CoinChange-2-MinimumNumberOfCoinsReqd.cpp

--- DynamicProgramming/CoinChange-2-MinimumNumberOfCoinsReqd.cpp
+++ DynamicProgramming/CoinChange-2-MinimumNumberOfCoinsReqd.cpp
@@ -1,4 +1,27 @@
-// Approach 1 - 2D dp
+//Approach 1 - 1D DP - Preferred one
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int dp[amount+1];
+        for(int i=0; i<=amount; i++)
+            dp[i] = INT_MAX;
+        dp[0] = 0;
+        for (int i=1; i <= amount; i++) {
+            //cout<<i<<" ";
+            for(int j=0; j<n; j++) {
+                //cout<<j;
+                if(coins[j] <= i and dp[i-coins[j]] != INT_MAX)
+                    dp[i] = min(dp[i-coins[j]]+1, dp[i]);
+            }
+        }
+        // for(int i=0; i<=amount; i++)
+        //     cout<<dp[i]<<" ";
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
+
+// Approach 2 - 2D dp
 class Solution {
     int knapsack(vector <int>wt, int sum, int n) {
         int tp[n+1][sum+1];
