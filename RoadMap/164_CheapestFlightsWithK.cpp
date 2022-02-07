9bcc855b55215192aad5e4987badc5b9dd771025
Anu Bansal <anubansal9999@gmail.com>
2022-02-07 23:19:10 +0530

    Update 164_CheapestFlightsWithK.cpp

--- RoadMap/164_CheapestFlightsWithK.cpp
+++ RoadMap/164_CheapestFlightsWithK.cpp
@@ -1,3 +1,4 @@
+//TLE
 typedef pair<int, pair<int, int>> ppi;
 class Solution {
 public:
@@ -33,5 +34,45 @@ public:
             }
         }
         return -1;
+    }
+};
+
+// More optimised
+#define ppi pair<int, pair<int, int>>
+class Solution {
+public:
+    int djikstra(vector<vector<pair<int, int>>> &adjList, int src, int dest, int k) {
+        priority_queue<ppi, vector<ppi>, greater<ppi>> nodeDist;
+        nodeDist.push({0, {src, -1}});
+        int n = adjList.size();
+        vector<int> visited(n, 0);
+        while(!nodeDist.empty()) {
+            ppi cur = nodeDist.top();
+            nodeDist.pop();
+            int curWt = cur.first;
+            int node =  cur.second.first;
+            int stopsDone = cur.second.second;
+            //cout<<"node"<<node;
+            if(visited[node] and visited[node] < stopsDone+1)
+                continue;
+             if(node == dest and stopsDone <= k)
+                return curWt;
+            visited[node] = stopsDone+1;
+            for(int i=0; i<adjList[node].size(); i++) {
+                int child = adjList[node][i].first;
+                int childWt = adjList[node][i].second;
+                 nodeDist.push({curWt + childWt, {child, stopsDone + 1}});  
+            }
+        }
+        return -1;
+    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        int edges = flights.size();
+        vector<vector<pair<int, int>>> adjList(n);
+        for(int i=0; i<edges; i++) {
+            adjList[flights[i][0]].push_back(make_pair(flights[i][1], flights[i][2]));
+        }
+        return djikstra(adjList, src, dst, k);
+        
     }
 };
