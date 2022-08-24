47ed2098e47ecbd4b57177fe4d53f9af1afad47c
Anu Bansal <anubansal9999@gmail.com>
2022-08-24 23:15:18 +0530

    Update 163_Djikstra.cpp

--- RoadMap/163_Djikstra.cpp
+++ RoadMap/163_Djikstra.cpp
@@ -3,37 +3,34 @@
 #include <queue>
 using namespace std;
 
-void djikstra1(int source, int n, vector<vector<int>> adjList[], vector <bool> &visited, vector <int> &distance) {
+void djikstra1(int source, int n, vector<vector<int>> adjList[], vector <int> &distance) {
 	// Min heap would be used to pop min distance element from the queue always
 	priority_queue <pair<int, int>, vector<pair<int, int> >, greater <pair<int, int> > > minHeap;
 	// Push source and it's distance pair into minHeap (Having distance as first element and node as second)
-	minHeap.push({distance[source], source});
+	minHeap.push({0, source});
 	// Run algo till que is not empty
 	while(!minHeap.empty()) {
 		// extracting minimum distance having node from heap
 		pair <int, int> current = minHeap.top();
 		minHeap.pop();
 		// If the popped node is already visited, then discard it
-		if(visited[current.second] == 1) {
+		if(distance[current.second] != INT_MAX) {
 			continue;
 		}
-		// node will be marked as visited at the time of poppping
-		visited[current.second] = 1;
+ 		// node will be marked as visited at the time of poppping
 		// popped distance will be the final minimum distance for this node
 		distance[current.second] = current.first; 
 		// exploring children of popped element
 		for (int i=0; i<adjList[current.second].size(); i++) {
 			int childNode = adjList[current.second][i][0];
 			// If it is not already visited
-			if(visited[childNode] == 0) {
+			if(distance[childNode] == INT_MAX) {
 				int wt = adjList[current.second][i][1];
 				minHeap.push({distance[current.second]+wt, childNode});
-			} 
-		}
-	}
-
-}
-};
+			    } 
+		    }
+	    }
+    }
 
 int main(int argc, char const *argv[])
 {
@@ -54,13 +51,12 @@ int main(int argc, char const *argv[])
 		adjList[x].push_back({y, wt});
 	}
 	int source = 0;
-	vector <bool> visited(n, 0);
 	// Initialising the distance array with infinity
 	vector <int> distance(n, INT_MAX);
 	// Making distance of source from source = 0
 	distance[source] = 0;
 
-	djikstra(source, n, adjList, visited, distance);
+	djikstra(source, n, adjList, distance);
 	for (auto num: distance) {
 		cout<<num;
 	}
